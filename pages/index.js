d5d9703c675a8c6531c0c65f8550d03bf7fa37fa
munib <info@smart387.ai>
2022-01-04 22:41:24 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ function Crypto() {
       {data.map((single, index) => (
         <div key={index} className='flex px-8 border-b divide-x-2'>
           <div className='w-1 mr-4'>{index == 0 ? ' ' : index}</div>
-          <div className='px-4 mx-4 truncate w-44'>{single.coin}</div>
+          <div className='w-32 px-4 mx-4 truncate sm:w-44'>{single.coin}</div>
           <div className='px-4'>{single.price}</div>
         </div>
       ))}
